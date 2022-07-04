19e595dde03620a19e32d18290ccc579d8fdc71a
KostuX <goldiskiker@gmail.com>
2022-07-04 16:03:26 +0100

    Ip range function

--- GetIpList.java
+++ GetIpList.java
@@ -7,7 +7,7 @@ public class GetIpList {
     public ArrayList<InetAddress> getIpList( int[] ip , int[] endIp, int timeout ) throws IOException {
 
         ArrayList<InetAddress> ipArrayList = new ArrayList<>();
-
+scan();
         int totalFound= 0;
 
         try {
@@ -39,6 +39,27 @@ public class GetIpList {
     catch (UnknownHostException uhe) {System.out.println("\nUnknown Host Exception\n"+uhe.getStackTrace());}
 
     return ipArrayList;
+    
+    
 
     }
+    
+    public static long ipToLong(InetAddress ip) {
+        byte[] octets = ip.getAddress();
+        long result = 0;
+        for (byte octet : octets) {
+            result <<= 8;
+            result |= octet & 0xff;
+        }
+        return result;
+    }
+
+    public static void scan() throws UnknownHostException {
+        long ipLo = ipToLong(InetAddress.getByName("192.200.0.0"));
+        long ipHi = ipToLong(InetAddress.getByName("192.255.0.0"));
+        long ipToTest = ipToLong(InetAddress.getByName("192.200.3.0"));
+
+        System.out.println(ipToTest >= ipLo && ipToTest <= ipHi);
+    }
+    
 }
