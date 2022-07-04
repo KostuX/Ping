bb57c76ff214229cd4045b5d20f1309b268fdf0b
Kostas9999 <91334044+Kostas9999@users.noreply.github.com>
2022-07-04 19:12:41 +0100

    Signed-off-by: Kostas9999 <91334044+Kostas9999@users.noreply.github.com>

--- iPv4/IPv4.java
+++ iPv4/IPv4.java
@@ -1,7 +1,28 @@
 package iPv4;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+
 public class IPv4 {
 	
-	public IPv4() {}
+    public static long ipToLong(InetAddress ip) {
+        byte[] octets = ip.getAddress();
+        long result = 0;
+        for (byte octet : octets) {
+            result <<= 8;
+            result |= octet & 0xff;
+        }
+        return result;
+    }
+
+    public static void scan() throws UnknownHostException {
+        long ipLo = ipToLong(InetAddress.getByName("192.200.0.0"));
+        long ipHi = ipToLong(InetAddress.getByName("192.255.0.0"));
+        long ipToTest = ipToLong(InetAddress.getByName("192.200.3.0"));
+
+        System.out.println(ipToTest >= ipLo && ipToTest <= ipHi);
+    }
 
 }
