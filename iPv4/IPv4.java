08648c2e78aa4172ef7236522375c6b679ad7855
Kostas9999 <91334044+Kostas9999@users.noreply.github.com>
2022-07-04 21:00:30 +0100

    IPRange

--- iPv4/IPv4.java
+++ iPv4/IPv4.java
@@ -1,5 +1,6 @@
 package iPv4;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -17,7 +18,7 @@ import java.util.List;
 public class IPv4 {	
 	
 
-List<InetAddress> ipRange = new ArrayList<>();
+static List<InetAddress> ipRange = new ArrayList<>();
 List<InetAddress> ipReachable = new ArrayList<>();
 
 
@@ -30,16 +31,35 @@ public static void test() throws UnknownHostException {
 	
 	System.out.println("Getting IPs");
 	
-	System.out.println("here");
+	
 	
 	  long ipLo = ipToLong(InetAddress.getByName("192.200.0.0"));
 	  
-	
 	  
-	for (long i = ipLo ; i < 300; i++) {
-		System.out.println( "dfdfdf"+ InetAddress.getByName("" + ipLo));
+	  InetAddress address = InetAddress.getByName("192.168.0.1");
+	  
+	  long addressLong = ipToLong(address);
+	  
+	  System.out.println(ipLo);
+	  
+	for (long i = 0 ; i < 300; i++) {
+	
+		IPv4.ipRange.add(InetAddress.getByName("" + addressLong++ ));	
+		
 		
 	}
+	ipRange.stream()
+	.filter(i->{
+		try {
+			return i.isReachable(200);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}).forEach(System.out::println);
+	
+	
 		
 	}
 
