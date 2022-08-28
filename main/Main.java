ff5fb591e90a2addf5e3af0fe3b61c14e2d249b5
KostuX <goldiskiker@gmail.com>
2022-08-29 00:35:42 +0100

    myIp

--- main/Main.java
+++ main/Main.java
@@ -3,14 +3,13 @@ package main;
 
 
 //@todo;
-//add response time 
+
 //get own ip;
 //get own network range;
-//get mac address vendor
-//get process for which have open specific port
-// read ports from file
 
-//threads
+//get process for which have open specific port
+//add response time 
+
 
 
 //https://www.printsupportcenter.com/hc/en-us/articles/115003386949-Determine-which-program-uses-or-blocks-a-port
@@ -26,13 +25,16 @@ import os.*;
 
 import java.net.*;
 import java.util.Date;
-
+import java.util.Enumeration;
 import java.util.ArrayList;
 import java.util.List;
 
 
 import host.Host;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
 public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
@@ -42,13 +44,15 @@ PortScan portScan = new PortScan();
 
 
 List<Host> onlineHosts = new ArrayList<>();	
-String ipString = "192.168.0.1";
+String ipString = "127.0.0.1";
 int scanHosts = 1;
 int timeout =500;
 
 	
 
 Date startTime = new java.util.Date();
+
+test();
 	  
 System.out.println("\n\t --- " + startTime +" ---" );
 System.out.println("\t\t --- Scann Started ---" );
@@ -88,6 +92,21 @@ time.TimeHelper.printTimeLapsed(startTime);
 
 }
 
-
+public static void test() throws IOException {
+    try (final DatagramSocket datagramSocket = new DatagramSocket()) {
+	    datagramSocket.connect(InetAddress.getByName("8.8.8.8"), 12345);
+	   System.out.println(datagramSocket.getLocalAddress().getHostAddress()); 
+	   
+	   String localHost = datagramSocket.getLocalAddress().getHostAddress();
+	   NetworkInterface networkInterface = NetworkInterface.getByInetAddress( InetAddress.getByName(localHost));
+	  System.out.println( networkInterface.getInterfaceAddresses().get(0).getNetworkPrefixLength());
+	}
+    
+    String urlString = "http://checkip.amazonaws.com/";
+    URL url = new URL(urlString);
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+        System.out.println(br.readLine());
+    }
+}
 
 }
